187c9f87207029c3f7f5fbe803e0870a3776cc75
yl951 <yl9517@naver.com>
2021-04-19 14:25:41 +0900

    Initial commit

---
+++ src/main/java/com/springweb/web/WebStart.java
@@ -0,0 +1,16 @@
+package com.springweb.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //해당 클래스에게 스프링 부트 데이터 제공
+public class WebStart {
+
+    //상속 : 상위클래스가 해당 클래스에게 변수와 메소드, 생성자 제공
+    //@어노테이션 : 하나의 프로그램을 제공
+    //장점 : 별도의 설치가 필요없음
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebStart.class, args);
+    }
+}
