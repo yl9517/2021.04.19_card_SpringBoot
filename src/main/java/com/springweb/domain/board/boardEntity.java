6502f6d97adf42abdb53592222a18f66c5251139
kingkang <lkh599810@naver.com>
2021-04-22 15:55:19 +0900

    2021.04.22 게시판 - domain-board

---
+++ src/main/java/com/springweb/domain/board/boardEntity.java
@@ -0,0 +1,66 @@
+package com.springweb.domain.board;
+
+import com.springweb.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity(name="board")
+public class boardEntity extends BaseTimeEntity {
+
+    @Id //@Id= 기본키 부여
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bbsID;
+
+    @Column(nullable = false)
+    private String bbsTitle;
+
+    @Column(nullable = false)
+    private String bbsCategory;
+
+    @Column(nullable = false)
+    private String bbsContent;
+
+    @Column(nullable = false)
+    private String bbsReply;
+
+    @Column(nullable = false)
+    private String userID;
+
+    @Builder
+    public boardEntity(Long bbsID, String bbsTitle, String bbsCategory, String bbsContent, String bbsReply, String userID){
+
+        this.bbsID=bbsID;
+        this.bbsTitle=bbsTitle;
+        this.bbsCategory=bbsCategory;
+        this.bbsContent=bbsContent;
+        this.bbsReply=bbsReply;
+        this.userID=userID;
+        
+    }
+
+    public void update(String bbsTitle, String bbsContent){
+        
+        this.bbsTitle=bbsTitle;
+        this.bbsContent=bbsContent;
+
+    }
+    
+    public void doneReply(){
+        
+        this.bbsReply="답변 완료";
+    }
+
+
+    public void undoReply(){
+        
+        this.bbsReply="답변 대기중";
+        
+    }
+
+
+}
