4fbf0edaa933528d2f84a4ddff5cec5ceb1e0487
leejehyun <lyjh98@gmail.com>
2021-04-29 15:56:40 +0900

    123

--- src/main/java/com/springweb/web/cardsController.java
+++ src/main/java/com/springweb/web/cardsController.java
@@ -1,4 +1,22 @@
 package com.springweb.web;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
 public class cardsController {
+
+    @GetMapping("/card/cards_page")
+    public String cards(){
+
+        return "cards_page";
+    }
+
+    public String cards_page(){
+
+        return
+    }
+
 }
