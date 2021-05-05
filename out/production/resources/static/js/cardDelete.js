7747af31d5f164dd01df9c6794bb4dbfe26d8d64
kingkang <lkh599810@naver.com>
2021-05-06 03:54:10 +0900

    문의게시판+ 관리자 카드등록/카드리스트

---
+++ out/production/resources/static/js/cardDelete.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    var id = $("#id").val();
+
+    $("#deleteBtn").click(function(){
+        $.ajax( {
+            type : 'DELETE' ,
+            url : '/admin/delete/'+id,
+            dataType : 'json',
+            contentType : 'application/json; charset=utf-8',
+            data : id,
+        }).done(function(data){
+            alert('카드가 삭제되었습니다.');
+            window.location.href="/admin";
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
+
+    });
+
+});
