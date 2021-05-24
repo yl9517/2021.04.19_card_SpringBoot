58fff4dcb4dbaa91694543f82a45019a3899a1da
yl951 <yl9517@naver.com>
2021-05-24 17:49:35 +0900

    카드 등록 value 비어있을 시 알림창 스크립트

--- src/main/resources/static/js/cardRegister.js
+++ src/main/resources/static/js/cardRegister.js
@@ -3,15 +3,17 @@ $(document).ready(function(){ //해당 문서
 
     function dosubmit(){
 
-        if($("#cardPhonto").val()==''){//카드이미지가 비어있으면
+        if($("#cardPhonto").val()==''){//카드이미지가 비어있으면..? => 아님. 명령어 바꿀것
             alert("카드 이미지가 비어있습니다.")
             return false;
         }
-        else if(){
+        else if($("#cardName").val()==''){
+            alert("카드 이름을 입력해주세요.")
+            return false;
         }
         else{
             $("#form").first().submit();
         }
     }
 
-})
+});
